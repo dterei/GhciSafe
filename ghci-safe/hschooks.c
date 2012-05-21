03c3b80c1b24a3cbdb9f903f0cde3e81fdbf25dd
David Terei <davidterei@gmail.com>
2012-05-21 12:07:30 -0700

    initial commit

---
+++ ghci-safe/hschooks.c
@@ -0,0 +1,39 @@
+/*
+These routines customise the error messages
+for various bits of the RTS.  They are linked
+in instead of the defaults.
+*/
+
+#include "../rts/PosixSource.h"
+#include "Rts.h"
+
+#include "HsFFI.h"
+
+#include <string.h>
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+void
+defaultsHook (void)
+{
+    RtsFlags.GcFlags.heapSizeSuggestion = 6*1024*1024 / BLOCK_SIZE;
+    RtsFlags.GcFlags.maxStkSize         = 512*1024*1024 / sizeof(W_);
+    RtsFlags.GcFlags.giveStats = COLLECT_GC_STATS;
+
+    // See #3408: the default idle GC time of 0.3s is too short on
+    // Windows where we receive console events once per second or so.
+#if __GLASGOW_HASKELL__ >= 703
+    RtsFlags.GcFlags.idleGCDelayTime = SecondsToTime(5);
+#else
+    RtsFlags.GcFlags.idleGCDelayTime = 5*1000;
+#endif
+}
+
+void
+StackOverflowHook (lnat stack_size)    /* in bytes */
+{
+    fprintf(stderr, "GHC stack-space overflow: current limit is %zu bytes.\nUse the `-K<size>' option to increase it.\n", (size_t)stack_size);
+}
+
